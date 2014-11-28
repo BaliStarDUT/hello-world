f33fac4fcdd3f8ea1d16fadab9bce8d9647c25d7
Yz <BaliStar-DUT@users.noreply.github.com>
2014-11-28 12:58:43 +0800

    Create GCdemo.java
    Demonstrate garbage collection and the finalize() method

---
+++ GCdemo.java
@@ -0,0 +1,25 @@
+//try this 4_2
+//Demonstrate garbage collection and the finalize() method
+class MyClass{
+	int x;
+	MyClass(int i){
+		x=i;
+		//System.out.println("Generate: "+x);
+	}
+	//called when object is recycled.
+	protected void finalize(){
+		System.out.println("Finalizing "+x);
+	}
+	//genarates an object that is immediately abandoned
+	void generate(int i){
+		MyClass o= new MyClass(i);
+	}
+	
+}
+class GCdemo{
+	public static void main(String[] args){
+		MyClass ob=new MyClass(0);
+		for(int count=1;count<1000000;count++)
+		ob.generate(count);
+	}
+}
