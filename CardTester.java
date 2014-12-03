3aa2b409a4c08136190ccaa5bb0c4ccfae58493a
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-03 12:14:51 +0800

    Update CardTester.java
    //第四章 习题 14,15

--- CardTester.java
+++ CardTester.java
@@ -1,4 +1,4 @@
-//第四章 习题 14
+//第四章 习题 14,15
 class Card{
 	int rank;
 	char suit;
@@ -17,5 +17,12 @@ class CardTester{
 	public static void main(String[] args){
 		Card card1=new Card(1,'A');
 		System.out.println("card1 rank= "+card1.getRank()+" suit= "+card1.getSuit());
+		Card card2=card1;
+		card2.rank=2;
+		card2.suit='B';
+		System.out.println("card1 rank= "+card1.getRank()+" suit= "+card1.getSuit());
+		System.out.println("card2 rank= "+card2.getRank()+" suit= "+card2.getSuit());
+
+		
 	}
 }
