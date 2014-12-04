136ab6493a4defae0ecef18103a1ccda0fd824b7
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-04 09:42:03 +0800

    Create SocketDemo.java
    Demonstrate Socket

---
+++ SocketDemo.java
@@ -0,0 +1,29 @@
+//Demonstrate Socket.
+import java.net.*;
+import java.io.*;
+class SocketDemo{
+	public static void main(String[] args){
+		int ch;
+		Socket socket=null;
+		try{
+		socket =new Socket("localhost",80);
+		InputStream in = socket.getInputStream();
+		OutputStream out=socket.getOutputStream();
+		String str=(args.length==0?"index.html":args[0])+"\n";
+		byte[] buf = str.getBytes();
+		out.write(buf);
+		while((ch=in.read())!=-1){
+				System.out.print((char)ch);
+		}
+		}catch(IOException exc){
+			System.out.println(exc);
+		}finally{
+			try{
+				if(socket!=null) socket.close();
+			}catch(IOException exc){
+				System.out.println("Error closing socket: "+exc);
+			}
+		}
+	}
+}
+
