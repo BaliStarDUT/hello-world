8f87b9ddabdad2f8e432e938f695d5b95f02f37c
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-05 21:02:17 +0800

    Create ForEach.java
    for-each遍历多维数组

---
+++ ForEach.java
@@ -0,0 +1,19 @@
+//Use for-reach style for on a two-dimensional array
+class ForEach2{
+public static void main(String[] args){
+	int sum=0;
+	int[][] nums=new int[3][5];
+	
+	for(int i=0;i<3;i++)
+		for(int j=0;j<5;j++)
+		nums[i][j]=(i+1)*(j+1);
+		
+	for(int[] x:nums){
+		for(int y:x){
+			System.out.println("Value is:"+y);
+			sum+=y;
+		}
+		}
+		System.out.println("Summation:"+sum);
+}
+}
