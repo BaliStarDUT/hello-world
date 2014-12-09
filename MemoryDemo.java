6415560ffbb12ecfbe92a74dd0280052414f3fb4
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-09 22:47:32 +0800

    Create MemoryDemo.java
    Class Runtime.Demostrate totalMemory(),freeMemory() and gc()

---
+++ MemoryDemo.java
@@ -0,0 +1,20 @@
+//Demostrate totalMemory(),freeMemory() and gc()
+class MemoryDemo{
+	public static void main(String[] args){
+		Runtime r=Runtime.getRuntime();
+		long mem1,mem2;
+		Integer[] someints=new Integer[1000];
+		System.out.println("Total memory is: "+r.totalMemory());
+		mem1=r.freeMemory();
+		System.out.println("Initial free memory: "+mem1);
+		r.gc();
+		mem1=r.freeMemory();
+		System.out.println("Free memory after garbage collection :"+mem1);
+		for(int i=0;i<1000;i++){
+			someints[i]=new Integer(i);
+		}
+		mem2=r.freeMemory();
+		System.out.println("Free memory after allocation:"+mem2);
+		System.out.println("Memory used by allocation is:"+(mem1-mem2));
+	}
+}
