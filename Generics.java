658f8d8fe878d6ea0fe84d48f80c0cc8e7c2af11
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-11 11:35:09 +0800

    Update Generics.java
    A simple generic class with two type parameters:T and V.

--- Generics.java
+++ Generics.java
@@ -1,34 +1,35 @@
-//A simple generic class .
-class Generics<T>{
-	T ob;
-	Generics(T o){
-		ob=o;
+//A simple generic class with two type parameters:T and V.
+class TwoGenerics<T,V>{
+	T ob1;
+	V ob2;
+	TwoGenerics(T o1,V o2){
+		ob1=o1;
+		ob2=o2;
 	}
-	T getob(){
-		return ob;
+	T getob1(){
+		return ob1;
 	}
-	void showType(){
-		System.out.println("Type of T is "+ob.getClass().getName());
+	V getob2(){
+		return ob2;
+	}
+	void showTypes(){
+		System.out.println("Type of T is "+ob1.getClass().getName());
+		System.out.println("Type of V is "+ob2.getClass().getName());
 	}
 }
 //Demonstrate the generic class
 class GenDemo{
 	public static void main(String[] args){
-		Generics<Integer> iOb;
-		iOb=new Generics<Integer>(88);
-		iOb.showType();
+		TwoGenerics<Integer,String> iOb;
+		iOb=new TwoGenerics<Integer,String>(88,"Generics");
 		
-		int v=iOb.getob();
+		iOb.showTypes();
+		
+		int v=iOb.getob1();
 		System.out.println("value: "+v);
-		
+		String str=iOb.getob2();
+		System.out.println("value: "+str);
 		System.out.println();
-		
-		Generics<String> strOb=new Generics<String>("Generics Test");
-		
-		strOb.showType();
-		
-		String str=strOb.getob();
-		System.out.println("Value:"+str);
 		
 	}
 }
