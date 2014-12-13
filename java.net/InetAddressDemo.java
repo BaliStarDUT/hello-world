03c9e248a3e1f4616f9d4f9e8e88b63aa6bf1079
Yz <BaliStar-DUT@users.noreply.github.com>
2014-12-13 21:08:54 +0800

    Update and rename InetAddressDemo.java to java.net/InetAddressDemo.java

--- java.net/InetAddressDemo.java
+++ java.net/InetAddressDemo.java
@@ -2,12 +2,20 @@
 import java.net.*;
 class InetAddressDemo{
 	public static void main(String[] args){
+		if(args.length!=1){
+			System.out.println("Usage: java InetAddressDemo URL");
+			return;
+		}
 		try{
 			InetAddress address=InetAddress.getByName("www.Google.com.hk");
 			System.out.println("Host name: "+address.getHostName());
 			System.out.println("Address: "+address.getHostAddress());
 			
 			System.out.println();
+			
+			InetAddress address2=InetAddress.getByName(args[0]);
+			System.out.println("Host name: "+address2.getHostName());
+			System.out.println("Address: "+address2.getHostAddress());
 			
 			
 		}catch(UnknownHostException exc){
