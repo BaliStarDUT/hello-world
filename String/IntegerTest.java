2d452588b766f3366208bc0569b48b718b6751da
Yz <BaliStarDUT@users.noreply.github.com>
2015-08-11 15:06:23 +0800

    Create IntegerTest.java
    基本数据类型和包装数据类的装箱与拆箱，相互转换

---
+++ String/IntegerTest.java
@@ -0,0 +1,26 @@
+public class  IntegerTest{
+    public static void main(String[] args) {
+        
+        // 定义double类型变量
+		double a = 91.5;
+        
+         // 手动装箱
+		Double b = new Double(a);     
+        
+        // 自动装箱
+		Double c =a;
+
+        System.out.println("装箱后的结果为：" + b + "和" + c);
+        
+        // 定义一个Double包装类对象，值为8
+		Double d = new Double(87.0);
+        
+        // 手动拆箱
+		double e = d.doubleValue();
+        
+        // 自动拆箱
+		double f =d;
+        
+         System.out.println("拆箱后的结果为：" + e + "和" + f);
+	}
+}
