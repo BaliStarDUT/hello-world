877e8132a9cd1c6dca89c12830f8d69a5567b310
Yz <BaliStarDUT@users.noreply.github.com>
2015-08-17 00:06:06 +0800

    Create CourseCellection.java
    测试数据类

---
+++ DataStructure/CourseCellection.java
@@ -0,0 +1,11 @@
+//package com.yang.test;
+/*课程类
+*/
+public class CourseCellection{
+	public String id;
+	public String name;
+	public CourseCellection(String id,String name){
+		this.id=id;
+		this.name=name;
+	}
+}
