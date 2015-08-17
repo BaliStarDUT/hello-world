717ea36e11c983db427a8910d2939fdf4ec61b79
Yz <BaliStarDUT@users.noreply.github.com>
2015-08-17 22:44:00 +0800

    Update CourseCellection.java
    添加无参构造器以实现子类的继承

--- DataStructure/CourseCellection.java
+++ DataStructure/CourseCellection.java
@@ -4,6 +4,9 @@
 public class CourseCellection{
 	public String id;
 	public String name;
+	public CourseCellection(){
+		
+	} 
 	public CourseCellection(String id,String name){
 		this.id=id;
 		this.name=name;
