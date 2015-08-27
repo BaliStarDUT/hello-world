ddf82bc67c87df6575a540ee39f19b910f38f2b4
Yz <BaliStarDUT@users.noreply.github.com>
2015-08-27 13:57:44 +0800

    Update JdbcDemo1.java
    使用excuteUpdate(String sql)方法完成数据的增删改删操作

--- JDBC/JdbcDemo1.java
+++ JDBC/JdbcDemo1.java
@@ -11,6 +11,7 @@ public class JdbcDemo1 {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		//要连接的数据库URL
         String url = "jdbc:mysql://localhost:3306/sushe";
+        System.out.println("The database you will connect is :"+url);
         //连接的数据库时使用的用户名
         String username = "root";
         //连接的数据库时使用的密码
@@ -26,10 +27,12 @@ public class JdbcDemo1 {
         Statement st = conn.createStatement();
         
         String sql = "select Student_ID,Student_Name,Student_Password,Student_Class,Student_Sex from student";
+        System.out.println("The sql is:"+sql);
         //4.向数据库发sql,并获取代表结果集的resultset
         ResultSet rs = st.executeQuery(sql);
         
         //5.取出结果集的数据
+        System.out.println("The result is:");
         while(rs.next()){
             System.out.println("Student_ID=" + rs.getObject("Student_ID"));
             System.out.println("Student_Name=" + rs.getObject("Student_Name"));
@@ -37,8 +40,16 @@ public class JdbcDemo1 {
             System.out.println("Student_Class=" + rs.getObject("Student_Class"));
             System.out.println("Student_Sex=" + rs.getObject("Student_Sex"));
         }
-        
+        //6.使用excuteUpdate(String sql)方法完成数据的添加操作
+        String sql2="insert into student(Student_DomitoryID,Student_Username,Student_Password,Student_Name,Student_Sex,Student_Class,Student_State)"
+        		+ "values(1,'001','123','yang','男','信管1101','入住')";
+        System.out.println(">Insert into a new list:"+sql2);
+        int num = st.executeUpdate(sql2);
+        if(num>0){
+        	System.out.println("Insert Success!!");
+        }
         //6.关闭链接，释放资源
+        System.out.println("Now close the connection!");
         rs.close();
         st.close();
         conn.close();
