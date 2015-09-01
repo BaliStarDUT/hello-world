9a9f3d3335485b71eb6f188f6e0da1c643a0bdb2
Yz <BaliStarDUT@users.noreply.github.com>
2015-09-01 23:51:29 +0800

    Update CourseCellection.java

--- DataStructure/CourseCellection.java
+++ DataStructure/CourseCellection.java
@@ -11,4 +11,28 @@ public class CourseCellection{
 		this.id=id;
 		this.name=name;
 	}
+	@Override //equals()重写模板
+	public boolean equals(Object obj){
+		if(this == obj){
+			return true;
+		}
+		if(obj == null)
+			return false;
+		if(!(obj instanceof CourseCellection))
+			return false;
+		CourseCellection course = (CourseCellection)obj;
+		if(this.name == null){
+			if(course.name == null){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			if(this.name.equals(course.name)){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
 }
