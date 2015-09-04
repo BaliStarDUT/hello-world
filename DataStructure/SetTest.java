71f5e8ed313d89d49bede1c662b8ce1119811fbc
Yz <BaliStarDUT@users.noreply.github.com>
2015-09-04 22:26:23 +0800

    Update SetTest.java
    测试Set的contains方法

--- DataStructure/SetTest.java
+++ DataStructure/SetTest.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class SetTest{
 	public List<CourseCellection> coursesToSelect;
+	public Student student;
+	private Scanner console;
 	public SetTest(){
 		coursesToSelect = new ArrayList<CourseCellection>();
+		console =new Scanner(System.in);
 	}
 	public void testAdd(){
 		//创建一个课程对象，并通过调用add方法，添加到备选课程List中
@@ -57,32 +60,53 @@ public class SetTest{
 	//打印输出是否包含该对象
 	System.out.println("Get the Course:"+course.name);
 	System.out.println("Contains this Course?"+course.name+","+coursesToSelect.contains(course));
-	CourseCellection course2 = new CourseCellection(course.id,course.name);
+	//提示输入课程名称
+	System.out.println("请输入课程名称：");
+	String name = console.next();
+	CourseCellection course2 = new CourseCellection();
+	course2.name =name;
 	System.out.println("New Course:"+course2.name);
 	System.out.println("Contains this Course?"+course2.name+","+coursesToSelect.contains(course2));
+	//通过indexOf()方法来获得某元素索引的位置
+	if(coursesToSelect.contains(course2))
+		System.out.println("课程："+course2.name+"的索引位置为："+coursesToSelect.indexOf(course2));
 }
-	public static void main(String[] args){
-	SetTest st = new SetTest();
-	st.testAdd();
-	st.testForEach();
+//创建学生对象并选课 
+	public void createStudentAndSelectCours(){
 	//创建一学生对象
-	Student student = new Student("1","Michal");
+	student = new Student("1","Michal");
 	System.out.println("欢迎学生："+student.name+"选课！");
 	//创建一个Scanner对象，用来接收从键盘输入的ID
 	Scanner console = new Scanner(System.in);
 	for(int i=0;i<3;i++){
 		System.out.println("请输入课程ID:");
 		String courseId = console.next();
-		for(CourseCellection cr:st.coursesToSelect){
+		for(CourseCellection cr:coursesToSelect){
 			if(cr.id.equals(courseId)){
 				student.courses.add(cr);
 				
 			}
 		}
 	}
-		st.testForEachForSet(student);
-		st.testListContains();
+		//st.testForEachForSet(student);
+}
+//测试Set的contains方法 
+	public void testSetContains(){
+		System.out.println("Please input the name of course:");
+		String name = console.next();
+		CourseCellection course = new CourseCellection();
+		course.name = name;
+		System.out.println("Create new course:"+course.name);
+		System.out.println("备选课程中是否包含课程"+course.name+","+student.courses.contains(course));
 	}
+	public static void main(String[] args){
+	SetTest st = new SetTest();
+	st.testAdd();
+	st.testForEach();
+	st.testListContains();
+	//st.createStudentAndSelectCours();
+	//st.testSetContains();
+}
 	public void testForEachForSet(Student student){
 		System.out.println("共选择了："+student.courses.size()+"门课程");
 		for(CourseCellection cr:student.courses){
