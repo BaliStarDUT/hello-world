a9914c489ca85196fd7c6f833fe82b38d99c1450
Yz <BaliStarDUT@users.noreply.github.com>
2015-09-04 22:47:00 +0800

    Update Student.java
    重写equals()方法和hashCode()方法，以使Map.containsValue()可以自定义

--- DataStructure/Student.java
+++ DataStructure/Student.java
@@ -6,11 +6,42 @@ import java.util.HashSet;
 public class Student{
 	public String id;
 	public String name;
-	public Set courses;
+	public Set<CourseCellection> courses;
 	public Student(String id,String name){
 		this.id=id;
 		this.name=name;
-		this.courses = new HashSet();
+		this.courses = new HashSet<CourseCellection>();
+	}
+	@Override
+	public int hashCode(){
+		final int prime =31;
+		int result = 1;
+		result = prime * result +((name == null)?0:name.hashCode());
+		return result;
+	}
+		@Override //equals()重写模板
+	public boolean equals(Object obj){
+		if(this == obj){
+			return true;
+		}
+		if(obj == null)
+			return false;
+		if(!(obj instanceof Student))
+			return false;
+		Student course = (Student)obj;
+		if(this.name == null){
+			if(course.name == null){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			if(this.name.equals(course.name)){
+				return true;
+			}else{
+				return false;
+			}
+		}
 	}
 	
 }
