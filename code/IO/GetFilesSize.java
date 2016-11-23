e39020e4d868b6ac1bf518f96eec89fb082b3c39
y0604 <yangz0604@dtdream>
2016-11-23 14:34:50 +0800

    update

---
+++ code/IO/GetFilesSize.java
@@ -0,0 +1,38 @@
+package IO;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.lang.management.ManagementFactory;
+//import com.sun.management.OperatingSystemMXBean;
+
+import org.junit.Test;
+
+/**
+ * 测试获取系统硬盘使用的例子
+ * 2016年11月23日 上午10:08:12
+ */
+public class GetFilesSize {
+	@Test
+	public void diskUseTest() {
+		File[] roots = File.listRoots();//获取磁盘分区列表     
+		for (File file : roots) {     
+			System.out.println(file.getPath()+"信息如下:");     
+		   System.out.println("空闲未使用 = " + file.getFreeSpace()/1024/1024/1024+"G");//空闲空间     
+		   System.out.println("已经使用 = " + file.getUsableSpace()/1024/1024/1024+"G");//可用空间     
+		   System.out.println("总容量 = " + file.getTotalSpace()/1024/1024/1024+"G");//总空间     
+		   System.out.println("空闲未使用 = " + file.getFreeSpace()/1024/1024+"M");//空闲空间     
+		   System.out.println("已经使用 = " + file.getUsableSpace()/1024/1024+"M");//可用空间     
+		   System.out.println("总容量 = " + file.getTotalSpace()/1024/1024+"M");//总空间     
+		   System.out.println("空闲未使用 = " + file.getFreeSpace());//空闲空间     
+		   System.out.println("已经使用 = " + file.getUsableSpace());//可用空间     
+		   System.out.println("总容量 = " + file.getTotalSpace());//总空间     
+		   System.out.println();
+		}     
+	}
+	@Test
+	public void memoryUseTest() {
+//		 OperatingSystemMXBean osmb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();     
+//	     System.out.println("系统物理内存总计：" + osmb.getTotalPhysicalMemorySize() / 1024/1024 + "MB");     
+//	     System.out.println("系统物理可用内存总计：" + osmb.getFreePhysicalMemorySize() / 1024/1024 + "MB");
+	}
+}
