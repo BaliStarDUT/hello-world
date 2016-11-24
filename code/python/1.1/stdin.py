f9be4f2d407264b933342b7fbfc8d857687020c1
james <drawnkid@gmail.com>
2016-11-24 22:50:31 +0800

    update

--- code/python/1.1/stdin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/python3
-# -*- coding: utf-8 -*-
-__author__='drawnkid@gmail.com'
-import sys
-fd = sys.stdin
-data = fd.read()
-print(data)
-sys.stdout.write(data)
-print('wosdfdjfafds dfa1w nihao ndif1')
