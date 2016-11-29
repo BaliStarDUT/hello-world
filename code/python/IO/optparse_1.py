fd13dffe1555fc47768d9d3ff86fbc198d6031f0
james <drawnkid@gmail.com>
2016-11-29 21:03:11 +0800

    update

--- code/python/IO/optparse_1.py
+++ code/python/IO/optparse_1.py
@@ -3,23 +3,43 @@
 __author__='drawnkid@gmail.com'
 from optparse import OptionParser
 import sys,os
-parser = OptionParser("Usage:%prog [file] [file2] ...")
-parser.add_option("-c","--chars",dest="chars",action="store_true",default=True,help="only count characters",)
-parser.add_option("-l","--lines",dest="lines",action="store_true",default  =True,help="only count lines")
-parser.add_option("-w","--wors",dest="words",action="store_true",default=True,help="only count words",)
+def options():
+    parser = OptionParser("Usage:%prog [file] [file2] ...")
+    parser.add_option("-c","--chars",dest="chars",action="store_true",default=True,help="only count characters",)
+    parser.add_option("-l","--lines",dest="lines",action="store_true",default  =True,help="only count lines")
+    parser.add_option("-w","--wors",dest="words",action="store_true",default=True,help="only count words",)
 
-options,args=parser.parse_args()
-print(options,args)
+    options,args=parser.parse_args()
+    print(options,args)
+    return options,args
 
-data = sys.stdin.read()
-chars = len(data)
-words = len(data.split())
-lines = data.count('\n')
-print(words,lines,chars)
+def get_count(data):
+    chars = len(data)
+    words = len(data.split())
+    lines = data.count('\n')
+    print(words,lines,chars)
+    return lines,words,chars
+def print_wc(options,lines,words,chars,fn):
+    if options.lines:
+        print("lines=",lines)
+    if options.words:
+        print("words=",words)
+    if options.chars:
+        print("chars=",chars)
+    print(fn)
 
-if options.chars:
-    print("chars=",chars)
-if options.words:
-    print("words=",words)
-if options.lines:
-    print("lines=",lines)
+def main():
+    options,args = options()
+    if args:
+	fn = args[0]
+	with open(args[0]) as fd:
+	    data = fd.read()
+    else:
+	fn = ""
+	print("Please input what to wc:")
+	data = sys.stdin.read()
+
+    lines,words,chars = get_count(data)
+    print_wc(options,lines,words,chars,fn)
+
+main()
