dcbc9183d753b87aa5f4b4a7fe57d631acb94e0a
james <drawnkid@gmail.com>
2016-12-04 22:14:53 +0800

    update

--- code/python/hashlib/hashlib_2.py
+++ code/python/hashlib/hashlib_2.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env/python3
 # -*- coding: utf-8 -*-
 __author__='drawnkid@gmail.com'
+import sys,os
 import hashlib
-import os,sys
-def md5sum(fileName):
-    md5 = hashlib.md5()
-    fd = open(fileName)
-    while True:
-        data = fd.read(1024*4)
-        if data:
-            md5.update(data)
-        else:
-            break
-    fd.close()
-    return md5.hexdigest()
 def md5sum2(fileName):
     md5 = hashlib.md5()
     with open(fileName) as fd:
@@ -24,23 +13,25 @@ def md5sum2(fileName):
             else:
                 break
     return md5.hexdigest()
-def file_md5(topdir):
+
+def gen_dic_file_md5(topdir):
+    dic ={} 
     a = os.walk(topdir)
     for p,d,f in a:
         for i in f:
             fn =os.path.join(p,i)
             md5 = md5sum2(fn)
-            yield(md5,fn)
-
+            if dic.__contains__(md5):
+                dic[md5].append(fn)
+            else:
+                dic[md5] = [fn]
+    return dic
 if __name__=="__main__":
-    lines=''
     try:
-        gen = file_md5(sys.argv[1])
-        for i in gen:
-            lines+=str(i)+'\n'
-            #print(lines)
-            print(hashlib.md5(lines.encode('utf8')).hexdigest())
-            #print(i)
-#        print(md5sum2(sys.argv[1]))
+        dic = gen_dic_file_md5(sys.argv[1])
+        for k,v in dic.items():
+            if len(v)>1:
+                print(v)
     except IndexError:
         print("a file is needed.")
+
---
+++ code/python/yield/yield_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+__author__='drawnkid@gmail.com'
+def h():
+    print('one')
+    yield 1
+    print('two')
+    yield 2
+    print('three')
+    yield 3
