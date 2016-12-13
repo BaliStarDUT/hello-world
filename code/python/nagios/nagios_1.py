e2b6b82ad16e9ea0bad135d6921136ee2f18a452
y0604 <yangz0604@dtdream.com>
2016-12-13 21:38:24 +0800

    update

--- code/python/nagios/nagios_1.py
+++ code/python/nagios/nagios_1.py
@@ -3,12 +3,12 @@
 __author__='drawnkid@gmail.com'
 import os
 import sys
-import urllib,urllib2
+import urllib.request
 import json
 CUR_DIR = os.path.dirname(__file__)
 CONF_DIR = os.path.join(os.path.abspath(CUR_DIR),'hosts')
 if not os.path.exists(CONF_DIR):
-    os.mkdir(CONFF_DIR)
+    os.mkdir(CONF_DIR)
 HOST_TEMP = """ define host{
     use     linux_server
     host_name    %(hostname)s
@@ -17,7 +17,7 @@ HOST_TEMP = """ define host{
     }
 """
 url ='http://192.168.1.106:8000/hostinfo/getjson/'
-req = urllib2.urlopen(url)
+req = urllib.request.urlopen(url)
 data = json.loads(req.read())
 conf = ''
 for hg in data:
