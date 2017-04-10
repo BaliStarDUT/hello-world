ef22430040aa0b4e1120352866d4bc94eeaff2b7
JamesYang <drawnkid@gmail.com>
2017-04-10 11:33:05 +0800

    add spring-boot-admin-client

--- code/mvn/james-spring-boot-admin-client/src/main/java/com/james/springboot/controller/IndexController.java
+++ code/mvn/james-spring-boot-admin-client/src/main/java/com/james/springboot/controller/IndexController.java
@@ -1,23 +1,17 @@
-package com.james.springboot;
+package com.james.springboot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.Desktop;
+import java.awt.*;
 import java.net.URI;
 
 @RestController
-@EnableAutoConfiguration
-@EnableScheduling
 public class IndexController {
     private Logger logger = LoggerFactory.getLogger(getClass());
-
     @RequestMapping("/")
     String home() {
         return "Hello World!"+"你好世界";
@@ -37,9 +31,4 @@ public class IndexController {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(IndexController.class, args);
-    }
-
 }
