4ee5dcad2466f299d9ceb394436204aa87ce8925
JamesYang <drawnkid@gmail.com>
2017-04-10 11:13:57 +0800

    add spring-admin

---
+++ code/mvn/james-spring-boot-admin/src/main/java/com/james/springboot/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.james.springboot;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by James Yang on 2017/4/7 0007 上午 11:15.
+ */
+@EnableAutoConfiguration
+@EnableScheduling
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
