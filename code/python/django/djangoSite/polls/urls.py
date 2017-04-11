8ed5703409bea6091a5ce0b68efefae52dc5ccb3
james <drawnkid@gmail.com>
2017-04-11 22:59:51 +0800

    update

--- code/python/django/djangoSite/polls/urls.py
+++ code/python/django/djangoSite/polls/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 app_name= 'polls'
 urlpatterns = [
+    #ex: /polls/
     url(r'^$',views.index,name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
