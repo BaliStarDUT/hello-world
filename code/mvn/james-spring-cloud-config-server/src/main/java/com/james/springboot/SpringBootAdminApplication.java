9bc02dc6636ba00ac055f327fa82444bd45ecc3a
James Yang <drawnkid@gmail.com>
2017-04-12 11:10:00 +0800

    update

--- code/mvn/james-spring-cloud-config-server/src/main/java/com/james/springboot/SpringBootAdminApplication.java
+++ code/mvn/james-spring-cloud-config-server/src/main/java/com/james/springboot/SpringBootAdminApplication.java
@@ -2,13 +2,13 @@ package com.james.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by James Yang on 2017/4/7 0007 上午 11:15.
  */
 @EnableAutoConfiguration
-@EnableScheduling
+@EnableConfigServer
 public class SpringBootAdminApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootAdminApplication.class, args);
