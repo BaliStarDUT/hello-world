12285e321de7e1c225becbf8ba5860688af398ea
James Yang <drawnkid@gmail.com>
2017-04-13 10:41:02 +0800

    add web

--- code/mvn/james-spring-boot-admin/src/main/java/com/james/springboot/SpringBootAdminApplication.java
+++ code/mvn/james-spring-boot-admin/src/main/java/com/james/springboot/SpringBootAdminApplication.java
@@ -2,13 +2,13 @@ package com.james.springboot;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by James Yang on 2017/4/7 0007 上午 11:15.
  */
-@EnableAutoConfiguration
+@SpringApplication
 @EnableScheduling
 @EnableAdminServer
 public class SpringBootAdminApplication {
---
+++ code/mvn/james-spring-boot-core-web/src/main/java/com/james/springboot/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.james.springboot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+import java.net.URI;
+
+@RestController
+public class IndexController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!"+"你好世界";
+    }
+
+    @Scheduled(fixedDelay=5000)
+    public void  scheduledTask() {
+        logger.debug("Logger Level ：DEBUG "+"Hello World!");
+        logger.info("Logger Level ：INFO "+"163");
+        try {
+            if(Desktop.isDesktopSupported()){
+                logger.warn("Logger Level ：WARN "+"Open browser");
+                Desktop.getDesktop().browse(new URI("http://music.163.com/outchain/player?type=0&id=71889585&auto=0&height=430"));
+            }
+        } catch (Exception e) {
+            logger.error("Logger Level ：ERROR "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
