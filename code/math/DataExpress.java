945ac068efe8d0caab034c127e8775fa7fc17489
Yz <BaliStarDUT@users.noreply.github.com>
2018-09-21 17:07:09 +0800

    update

---
+++ code/math/DataExpress.java
@@ -0,0 +1,9 @@
+public class DataExpress {
+  public static void main(String []args){
+    System.out.println("Integer.MAX_VALUE:"+Integer.MAX_VALUE);
+    System.out.println("Integer.MIN_VALUE:"+Integer.MIN_VALUE);
+    System.out.println("Integer.MAX_VALUE:"+Integer.MAX_VALUE);
+    System.out.println("Integer.MAX_VALUE:"+Integer.MAX_VALUE);
+
+  }
+}
